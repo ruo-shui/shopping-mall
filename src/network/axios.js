9e37e46a71bf907b0258283627034fc92e0bad83
ruo-shui <814696331@qq.com>
2020-07-03 00:53:10 +0800

    '完成全部页面'

--- src/network/axios.js
+++ src/network/axios.js
@@ -4,7 +4,7 @@ export default function axios(option) {
   return new Promise((resolve, reject) => {
     //创建axios的实例
     const instance = originAxios.create({
-      baseURL: 'http://152.136.185.210:8000/api/n3',
+      baseURL: 'http://123.207.32.32:8000',
       timeout: 5000
     })
 
